5a4ddaca72a1ebe652bf7de5fdbdb7eef4752926
ashlinhanson <ashlinhanson@gmail.com>
2020-08-23 17:33:29 -0500

    link refs, api url

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//day variables
+var currentDate = moment().format('M/DD/YYYY');
+var 
+
+$(document).ready(function() {
+
+}
+) 
+
+
+// api key url
+http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={e96dc3bf6b9350e78107e685794c2a31}
