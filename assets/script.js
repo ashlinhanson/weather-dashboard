1813400c8cb098ff4bea3a51347f0f21c3c3f2a4
ashlinhanson <ashlinhanson@gmail.com>
2020-08-24 12:21:44 -0500

    more wireframing with HTML

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 //day variables
 var currentDate = moment().format('M/DD/YYYY');
-var 
 
 $(document).ready(function() {
 
@@ -9,4 +8,4 @@ $(document).ready(function() {
 
 
 // api key url
-http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={e96dc3bf6b9350e78107e685794c2a31}
+//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={e96dc3bf6b9350e78107e685794c2a31}
