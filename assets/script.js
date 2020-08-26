597a65336296a27cd7216d2104a733cab630332b
ashlinhanson <ashlinhanson@gmail.com>
2020-08-26 18:33:18 -0500

    daily done, five day error 400

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,24 @@
 //day variables
 var currentDate = moment().format("L");
+let day1 = moment().add(1, 'day').format('L');
+let day2 = moment().add(2, 'day').format('L');
+let day3 = moment().add(3, 'day').format('L');
+let day4 = moment().add(4, 'day').format('L');
+let day5 = moment().add(5, 'day').format('L');
 var searchInput = $("searchInput");
 var currentDay = $("currentDiv");
+let city = $("#searchInput").val();
+var apiKey = "e96dc3bf6b9350e78107e685794c2a31";
 const farSym = "\u2109";
+
+
 $("#citySrchBtn").click(getWeather)
 
 $(".card").css("display", "none");
 
+
 function getWeather(){
     event.preventDefault();
-    var city = $("#searchInput").val();
-    var apiKey = "e96dc3bf6b9350e78107e685794c2a31";
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     var temperature; 
     
@@ -18,12 +26,11 @@ function getWeather(){
         url : queryURL,
         method : "GET"
     }).then(function(response){
-        console.log(response);
+        //console.log(response);
         temperature = changeTemp(response.main.temp);
         var humidity = response.main.humidity;
-        console.log(humidity)
+        //console.log(humidity)
         var wind = response.wind.speed;
-        console.log(wind)
         var iconCode = response.weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"; 
         var img = $(`<img src="${iconUrl}">`)
@@ -47,9 +54,9 @@ function getUVIndex(lat,lon){
         url : uvQueryURL,
         method : "GET"
     }).then(function(result){
-        console.log(result);
+        //console.log(result);
         var uvIndex = result.value;
-        console.log(uvIndex)
+        //console.log(uvIndex)
 
         if (uvIndex <= 3){
             $(".currentUVIndex").addClass("btn-success");
@@ -61,6 +68,19 @@ function getUVIndex(lat,lon){
         $(".currentUVIndex").append("UV Index: " + uvIndex)
     })
 }
+        var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function(result){
+            console.log(result);
+            // var forecastCodes = [result.list[1].weather[0].icon, 
+            // result.list[2].weather[0].icon,
+            // result.list[3].weather[0].icon,
+            // result.list[4].weather[0].icon,
+            // result.list[5].weather[0].icon];
+        });
 
 function changeTemp (kelvin) {
     var temp = parseInt(((kelvin - 273.15) * 1.80 + 32));
