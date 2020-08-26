9ae513451cff7988c4799aadea7986af0ce5f5ce
ashlinhanson <ashlinhanson@gmail.com>
2020-08-26 11:29:46 -0500

    some html styling, trying to get API call to populate

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 //day variables
 var currentDate = moment().format("L");
 var searchInput = $("searchInput");
-var currentDay = $("currentDay");
+var currentDay = $("currentDiv");
 const farSym = "\u2109";
 
 
-$("citySrchBtn").click(function(){
+$("#citySrchBtn").click(function(){
     event.preventDefault();
     var city = $("#searchInput").val();
     var apiKey = "e96dc3bf6b9350e78107e685794c2a31";
@@ -18,32 +18,38 @@ $("citySrchBtn").click(function(){
     }).then(function(response){
         console.log(response);
 
-        temperature = convertTemp(response.main.temperature);
+        temperature = changeTemp(response.main.temperature);
         var humidity = response.main.humidity;
         var wind = response.wind.speed;
 
         var iconCode = response.weather[0].icon;
-    })
+
+        var iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"; 
+        
+        
+
+       
+    
 
 
-$(".currentDay").addClass("border border-secondary");
-$(".city").text(city + " (" + currentDate + ") ");
+$(".currentDiv").addClass("border");
+$(".searchInput").text(searchInput + " (" + currentDate + ") ");
+
+$(".cityName").text(searchInput.val());
+$(".currentHumidity").text("Humidity: " + humidity + "%");
+$(".currentTemperature").text("Temperature: " + temperature + farSym);
+$(".currentWind").text("Wind Speed: " + wind + " MPH");
 
 
-$(".humidity").text("Humidity: " + humidity + "%");
-$(".temperature").text("Temperature: " + temperature + farSym);
-$(".wind").text("Wind Speed: " + wind + " MPH");
+})
 
 })
 
 
 
-function convertTemp (kelvin) {
+
+function changeTemp (kelvin) {
     var temp = Math.floor((kelvin - 273.15) * 1.80 + 32);
     return temp;
 }
 
-
-
-// api key url
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={e96dc3bf6b9350e78107e685794c2a31}
