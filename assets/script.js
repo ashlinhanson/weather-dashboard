e7972a481ed08a24e4f770c4af498e2ea447a04c
ashlinhanson <ashlinhanson@gmail.com>
2020-08-26 12:31:24 -0500

    UV index added

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,14 @@ var currentDate = moment().format("L");
 var searchInput = $("searchInput");
 var currentDay = $("currentDiv");
 const farSym = "\u2109";
+$("#citySrchBtn").click(getWeather)
 
-
-$("#citySrchBtn").click(function(){
+function getWeather(){
     event.preventDefault();
     var city = $("#searchInput").val();
     var apiKey = "e96dc3bf6b9350e78107e685794c2a31";
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    var temperature;
+    var temperature; 
     
     $.ajax({
         url : queryURL,
@@ -18,38 +18,54 @@ $("#citySrchBtn").click(function(){
     }).then(function(response){
         console.log(response);
 
-        temperature = changeTemp(response.main.temperature);
+        temperature = changeTemp(response.main.temp);
         var humidity = response.main.humidity;
+        console.log(humidity)
         var wind = response.wind.speed;
+        console.log(wind)
 
         var iconCode = response.weather[0].icon;
 
         var iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"; 
-        
+        var img = $(`<img src="${iconUrl}">`)
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+        getUVIndex(lat,lon)
         
 
        
     
 
 
-$(".currentDiv").addClass("border");
-$(".searchInput").text(searchInput + " (" + currentDate + ") ");
+// $(".currentDiv").addClass("border");
+$(".searchInput").append(searchInput + " (" + currentDate + ") ");
 
-$(".cityName").text(searchInput.val());
-$(".currentHumidity").text("Humidity: " + humidity + "%");
-$(".currentTemperature").text("Temperature: " + temperature + farSym);
-$(".currentWind").text("Wind Speed: " + wind + " MPH");
+$(".cityName").append(searchInput.val());
+$(".currentHumidity").append("Humidity: " + humidity + "%");
+$(".currentTemperature").append("Temperature: " + temperature + farSym);
+$(".currentWind").append("Wind Speed: " + wind + " MPH");
+$(".weatherIcon").append(img);
 
 
 })
 
-})
+}
+function getUVIndex(lat,lon){
+    var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=e96dc3bf6b9350e78107e685794c2a31&lat="+lat+"&lon="+lon;
+    $.ajax({
+        url : uvQueryURL,
+        method : "GET"
+    }).then(function(uvResponse){
+        console.log(uvResponse);
+        $(".currentUVIndex").append("UV Index " + uvResponse.value)
+    })
+}
 
 
 
 
 function changeTemp (kelvin) {
-    var temp = Math.floor((kelvin - 273.15) * 1.80 + 32);
+    var temp = parseInt(((kelvin - 273.15) * 1.80 + 32));
     return temp;
 }
 
